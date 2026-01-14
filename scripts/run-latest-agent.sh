3a0d35386968550672bc98d563cc2727989ec703
Itamar Holder <iholder@redhat.com>
2026-01-14 14:17:51 +0200

    add --pull=newer to run-latest-agent.sh
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- scripts/run-latest-agent.sh
+++ scripts/run-latest-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-podman run --rm \
+podman run --rm --pull=newer \
     quay.io/mabekitzur/vep-police-agent:latest \
     --api-key "$(cat API_KEY)" \
     --google-token "$(cat GOOGLE_TOKEN)"
